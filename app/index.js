4f974ab9d3c37a4109e1af42d0c2abfb69302a5c
Flaqueeau <econrad@zagmail.gonzaga.edu>
2017-03-26 10:37:21 -0700

    Move Main structure over to Main

--- app/index.js
+++ app/index.js
@@ -3,21 +3,9 @@ import { render } from 'react-dom';
 import 'app.global.css';
 import 'old.global.css';
 import 'skeleton.global.css';
-import HTMLRenderer from 'html_renderer.js';
-import DraggableHeader from 'draggable_header/draggable_header.js'
-import Editors from "editors/editors.js"
-
-function App(props) {
-  return (
-    <div id="app">
-      <DraggableHeader />
-      <HTMLRenderer html={'jilji'} css={''}/>
-      <Editors />
-    </div>
-  )
-}
+import Main from 'main/main.js'
 
 render(
-  <App />,
+  <Main />,
   document.getElementById('root')
 );
