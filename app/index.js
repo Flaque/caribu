4a13d432a20ec997e1da3a94f05c2bfb60f26356
Flaqueeau <econrad@zagmail.gonzaga.edu>
2017-03-26 10:16:23 -0700

    Add relative paths

--- app/index.js
+++ app/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { render } from 'react-dom';
-import './styles/app.global.css';
-import './styles/old.global.css';
-import './styles/skeleton.global.css';
-import HTMLRenderer from './components/html_renderer.js';
-import DraggableHeader from './components/draggable_header/draggable_header.js'
+import 'app.global.css';
+import 'old.global.css';
+import 'skeleton.global.css';
+import HTMLRenderer from 'html_renderer.js';
+import DraggableHeader from 'draggable_header/draggable_header.js'
 
 function App(props) {
   return (
