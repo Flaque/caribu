88d0768141d6afc0ffe5d905be2b2d85eee4b511
Flaqueeau <econrad@zagmail.gonzaga.edu>
2017-03-25 22:23:53 -0700

    Add editor

---
+++ app/components/editor/editor.js
@@ -0,0 +1,28 @@
+import CodeMirror from 'react-codemirror'
+import 'codemirror/lib/codemirror.css'
+
+const editorOptions = {
+  lineNumbers: true
+}
+
+class Editor extends React.Component {
+
+  getInitialState() {
+    return { code: "//Code" }
+  }
+
+  updateCode = (newCode) => {
+    this.setState({
+      code: newCode
+    })
+  }
+
+  render() {
+    return (
+      <CodeMirror
+        value={this.state.code}
+        onChange={this.updateCode}
+        options={editorOptions} />
+    )
+  }
+}
