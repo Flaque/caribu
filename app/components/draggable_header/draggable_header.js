140752f73c1d107ff3a02fa09f4e5c11242aa28b
Flaqueeau <econrad@zagmail.gonzaga.edu>
2017-03-26 10:11:48 -0700

    Add docs to draggable header

--- app/components/draggable_header/draggable_header.js
+++ app/components/draggable_header/draggable_header.js
@@ -1,3 +1,8 @@
+/**
+ * A header at the top of the screen that
+ * lets the user drag the electron window around,
+ * even when the window is frameless.
+ */
 import React from 'react'
 import styles from './draggable_header.css'
 
