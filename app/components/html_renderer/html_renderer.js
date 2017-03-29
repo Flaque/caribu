56394b5d057e5ec496593eb25935a6e8e2d32547
Flaqueeau <econrad@zagmail.gonzaga.edu>
2017-03-28 19:26:38 -0700

    Fix flashes of light

--- app/components/html_renderer/html_renderer.js
+++ app/components/html_renderer/html_renderer.js
@@ -1,30 +1,15 @@
 import React from 'react';
 import styles from './html_renderer.css'
 
-
-/**
- * Returns a json object that REACT needs to render stuff
- * dangerously.
- *
- * @param {*} rawHTML - the html to be inserted dangerously
- */
-function getDangerousHTML(rawHTML){
-    return {__html: rawHTML};
-}
-
-/**
- * This function will take in the raw css and html and format
- * them such that they can be inserted into an html json tag
- * for render on the frame.
- */
-function composeUserHTML(html, css){
-    var style = "data:text/html,<style>"+ css +"</style>";
-    return encodeURI(style + html);
-}
-
 class HTMLRenderer extends React.Component{
+
     render(){
-        return (<iframe className={styles.html_renderer} src={composeUserHTML(this.props.html, this.props.css)}/>);
+        return (<iframe id="output" className={styles.html_renderer}/>);
+    }
+
+    componentDidUpdate() {
+      const doc = document.getElementById('output').contentWindow.document
+      doc.body.innerHTML = `<style>${this.props.css}</style> ${this.props.html}`
     }
 }
 
